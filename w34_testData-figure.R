1eda8e46ae1f18fc7f1578d5d56ecc492f96183c
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-10-17 14:35:47 -0700

    axes labels

--- w34_testData-figure.R
+++ w34_testData-figure.R
@@ -4,24 +4,24 @@ w34 <- nc::capture_first_df(
   fread("w34_testData.csv"),
   bin=list(
     "\\(",
-    lower=".*", as.numeric,
+    height.lower=".*", as.numeric,
     ",",
-    upper=".*", as.numeric,
+    height.upper=".*", as.numeric,
     "\\]",
     prop=".*", as.numeric,
     "%"))
-w34[, mid := (lower+upper)/2]
+w34[, height.mid := (height.lower+height.upper)/2]
 
 table(w34$treeID)
 
-some.trees <- w34[treeID %in% c(1, 3, 13, 9, 11, 16, 19)]
+some.trees <- w34[treeID %in% c(1, 2, 3, 13, 9, 11, 16, 19)]
 
 gg.data <- ggplot()+
   theme_bw()+
   theme(panel.spacing=grid::unit(0, "lines"))+
-  facet_wrap("treeID")+
+  facet_wrap("treeID", labeller=label_both)+
   geom_point(aes(
-    mid, `95th_eucDist`),
+    `95th_eucDist`, height.mid),
     data=some.trees)
 
 g <- gfpop::graph(
@@ -35,23 +35,25 @@ seg.dt <- some.trees[, {
   data.table(
     i.start,
     i.end,
-    segStart=lower[i.start],
-    segEnd=upper[i.end],
+    height.start=height.lower[i.start],
+    height.end=height.upper[i.end],
     state=fit$states,
     mean=fit$parameters)
 }, by=treeID]
 
 gg.model <- gg.data+
   geom_segment(aes(
-    segStart, mean,
-    xend=segEnd, yend=mean),
+    mean, height.start,
+    xend=mean, yend=height.end),
     color="red",
     size=1,
     data=seg.dt)+
-  geom_vline(aes(
-    xintercept=segEnd),
+  geom_hline(aes(
+    yintercept=height.end),
     color="red",
-    data=seg.dt[i.start==1])
+    data=seg.dt[i.start==1])+
+  xlab("Distance/radius from trunk parallel to ground (meters)")+
+  ylab("Height from ground (meters)")
 
 png("w34_testData-figure.png", 6, 6, units="in", res=100)
 print(gg.model)
